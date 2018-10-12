f09690dd190abb9647d78c0a1850d626fa8eedeb
Laznet <charly2221@hotmail.fr>
2018-10-12 14:42:52 +0200

    Ajout du javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+var c = 0, a = 2, b = 3;
+
+c = a + b;
+
+console.log('Adition ' + a + ' + ' + b + ' = ' + c);
+
+
+c = a - b;
+
+console.log('Soustraction ' + a + ' - ' + b + ' = ' + c);
+
+
+c = a * b;
+
+console.log('Multiplication ' + a + ' x ' + b + ' = ' + c);
+
+c = a / b;
+
+console.log('Division ' + a + ' / ' + b + ' = ' + c);
